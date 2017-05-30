fb1203c8b3c0027b27c8d50be25c2b9499623b2b
ronakthamor <ronak.thamor@gmail.com>
2017-05-30 15:39:41 -0400

    typo

--- provision.sh
+++ provision.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 sudo apt-get install mailutils -y
 (crontab -l 2>/dev/null; echo "*/5 * * * * lsof -i :4567 | grep LISTEN || echo "Not listening" | mail help@regalii.com") | crontab -
 \curl -L https://get.rvm.io | sudo bash -s stable --ruby=2.4.1
-source /usr/local/rvm/scripts/rvm
+source /usr/local/rvm/
 gem install sinatra
 ruby /var/www/devops/app.rb -o 0.0.0.0
